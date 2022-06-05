42a3fd19abb3ff5ce7037a43d8cf805bc1e7d9c3
laozudamo <laozudamo@gmail.com>
2022-06-05 23:29:00 +0800

    feat: fill 填充数组 && concat数组拼接

---
+++ fillArr.js
@@ -0,0 +1,14 @@
+const arr = [1, 2, 4, 5]
+// const insert = [1, 2]
+// arr.fill('22', 0, 2)
+// console.log(arr)
+
+const arrb = [7, 8, 9]
+
+/* 
+  concat不会改变原数组
+*/
+const arrc = arr.concat(arrb)
+console.log(arr)
+console.log(arrb)
+console.log(arrc)
